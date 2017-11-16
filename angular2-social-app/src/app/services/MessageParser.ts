0a32b1ec8e42aa497045d8e06d8f3796efca03ee
Antonin Charrier <acharrier@intechinfo.fr>
2017-11-16 12:31:13 +0100

    Commentaires content

--- angular2-social-app/src/app/services/MessageParser.ts
+++ angular2-social-app/src/app/services/MessageParser.ts
@@ -3,7 +3,8 @@ import {
     PostContent,
     YoutubePostContent,
     PicturePostContent,
-    VideoPostContent
+    VideoPostContent,
+    BuiltMessage
 }
     from '../models';
 
@@ -16,6 +17,13 @@ const youtube = "https://youtu.be/";
  */
 export class MessageParser {
 
+    private messageArray: BuiltMessage[];
+    private urlRegex = new RegExp(
+        "(^|[ \t\r\n])((ftp|http|https|gopher|mailto|news|nntp|telnet|wais|file|prospero|aim|webcal):(([A-Za-z0-9$_.+!*(),;/?:@&~=-])|%[A-Fa-f0-9]{2}){2,}(#([a-zA-Z0-9][a-zA-Z0-9$_.+!*(),;/?:@&~=%-]*))?([A-Za-z0-9$_+!*();/?:~-]))"
+       ,"g"
+     );
+
+    
     parse(post: Post): PostContent<any> {
         const pictureRegex = /http[s]?:\/\/.+\.(jpeg|png|jpg|gif)/gmi;
         const pictureMatche = pictureRegex.exec(post.message);
@@ -33,5 +41,44 @@ export class MessageParser {
 
 
         return null;
+    }
+
+    buildMessage(message: string) : BuiltMessage[] {
+        this.messageArray = [];
+        let urlArray = message.match(this.urlRegex); 
+        if(urlArray)        
+            this.splitMessage(message, urlArray, 0);
+        else
+            this.messageArray.push({type: "text", text: message.trim()});
+        return this.messageArray;
+    }
+
+    private splitMessage(message: string, urlArray: string[], idx: number) : string[] {
+        const splittedMessage = message.split(urlArray[idx]);
+        if(splittedMessage[0].trim() !== "") {
+            this.messageArray.push({type: "text", text: splittedMessage[0].trim()});
+        }
+        if(urlArray[idx].trim() !== "") { 
+            if((/\.(gif|jpg|jpeg|tiff|png)$/i).test(urlArray[idx].trim())) {
+                this.messageArray.push({type: "picture", picture: new PicturePostContent(urlArray[idx].trim())});
+            } else if((/\.(avi|mov|mp4|webm|ogg|wav)$/i).test(urlArray[idx].trim())) {
+                this.messageArray.push({type: "video", video: new VideoPostContent(urlArray[idx].trim())});            
+            } else if ((/(http[s]?:\/\/)?www\.(?:youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\/?\?(?:\S*?&?v\=))|youtu\.be\/)([a-zA-Z0-9_-]{6,11})/gmi).test(urlArray[idx].trim())) {
+                let video_id = urlArray[idx].trim().split('v=')[1];
+                let ampersandPosition = video_id.indexOf('&');
+                if(ampersandPosition != -1) {
+                    video_id = video_id.substring(0, ampersandPosition);
+                }
+                this.messageArray.push({type: "youtube", youtube: new YoutubePostContent(video_id)});                        
+            } else {
+                this.messageArray.push({type: "link", link: urlArray[idx].trim()});                            
+            }
+        }
+        if(urlArray.length>idx+1 && splittedMessage[1].split(urlArray[idx +1].trim()).length > 1) {
+            this.splitMessage(splittedMessage[1], urlArray, idx+1);
+        } else {
+            if(splittedMessage[1].trim() !== "") this.messageArray.push({type: "text", text: splittedMessage[1].trim()});            
+        }
+        return splittedMessage;
     }
 }
